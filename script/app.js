842f07a9a3d0fbe5bc6b2cf433ac4a183f7f027f
Fabian <fabian9897@gmail.com>
2019-12-06 12:10:18 +0100

    adding basic layout for visual and test data

--- script/app.js
+++ script/app.js
@@ -1,3 +1,16 @@
-jQuery('.cover').click( function(e) {
-    jQuery('.collapse').collapse('hide');
+$('.cover').click( function(e) {
+    $('.collapse').collapse('hide');
 });
+
+if($(".image_container") !== null){
+    var image_container = $(".image_container");
+    $.getJSON('../data/imageCollections.json', function(data) {         
+        var imageCollections = data;
+        
+        // imageCollections.forEach(collection => {
+        //     var container = document.createElement("div");
+        //     container.innerHTML=collection.name;
+        //     document.getElementsByClassName("image_container")[0].appendChild(container);
+        // });
+    });
+};
