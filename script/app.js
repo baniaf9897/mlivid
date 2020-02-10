fcd6090c96cb16ddc6c31be3427f77b4eb592f31
Fabian <fabian9897@gmail.com>
2020-02-10 23:25:13 +0100

    adding btns and onclick

--- script/app.js
+++ script/app.js
@@ -3,14 +3,31 @@ $('.cover').click( function(e) {
 });
 
 if($(".image_container") !== null){
-    var image_container = $(".image_container");
+    var button_list = $(".vertical_list")[0];
+
     $.getJSON('../data/imageCollections.json', function(data) {         
         var imageCollections = data;
         
         imageCollections.forEach(collection => {
-            var container = document.createElement("div");
-            container.innerHTML=collection.name;
-            document.getElementsByClassName("image_container")[0].appendChild(container);
+            var container = document.createElement("li");
+            var button = document.createElement("button");
+          
+            button.addEventListener('click', function(){
+                log(collection);
+            });
+            button.innerText = collection.name;
+        
+            container.appendChild(button);
+            button_list.appendChild(container);
         });
     });
 };
+
+
+function log(collection){
+    var image_container = $(".image_container")[0];
+    image_container.style.backgroundImage = "url('" + collection.thumbnail + "')"
+    console.log("Hello btn : ", collection.id);
+
+
+}
