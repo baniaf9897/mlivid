61946c3d65ed29c42d7735731436c4be3cc4b1ff
Fabian <fabian9897@gmail.com>
2020-02-11 09:20:10 +0100

    adding images

--- script/app.js
+++ script/app.js
@@ -26,8 +26,24 @@ if($(".image_container") !== null){
 
 function log(collection){
     var image_container = $(".image_container")[0];
+    var heading = $(".collection_heading")[0];
+    var description = $(".collection_description")[0];
+    var collection_images = $(".collection_images")[0];
+
     image_container.style.backgroundImage = "url('" + collection.thumbnail + "')"
-    console.log("Hello btn : ", collection.id);
+    heading.innerText = collection.name;
+    description.innerText = collection.description;
 
 
+    collection.images.forEach(image =>{
+
+        var image_element_container = document.createElement("div");
+        image_element_container.classList += "tile";
+        var image_element = document.createElement("img");
+        image_element.src  = image;
+        image_element.classList += "cover";
+        
+        image_element_container.appendChild(image_element);
+        collection_images.appendChild(image_element_container);
+    });
 }
